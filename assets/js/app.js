7d7584cce66777d7e9f230b11f51b7d5053d335b
zzxp <zzxp111gmail.com>
2022-08-16 22:01:20 -0500

    0.2_pedir_carta

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,59 @@
 
 let deck = [];
 const tipo = ['C','D','H','S'];
-
+const especial = ['A','J','Q','k'];
 const crearDeck = ()=>{
-    for( let i = 2; i <= 10; i++){
-        for (let t of tipo){
+    for(let t of tipo){
+        for (let i = 2; i <= 10; i++){
 
-            deck.push(i+tipo);
+            deck.push(i+t);
 
         }        
     }
+
+    for(let t of tipo){
+        for(let e of especial){
+            
+            deck.push(e+t);
+        }
+    }
+    //console.log(deck);
+    deck = _.shuffle(deck);
     console.log(deck);
+    return deck;
 }
 
 crearDeck();
+
+// Pedir carta
+const pedirCarta = ()=>{
+    if(deck.length>0){
+        let carta = deck.pop();
+        console.log(carta);
+        console.log(deck);
+        return carta;        
+    }else{
+        throw('No quedan cartas en el deck');
+    }
+}
+
+
+
+//Valor Carta
+
+const valorCarta = (carta)=>{
+    let valor = carta.substring(0,carta.length-1);
+    console.log(valor);
+    if(isNaN(valor)){
+        console.log('No es un numero');
+        valor = (valor==='A') ?11:10;
+    }else{
+        console.log('Es un numero');
+        valor = valor * 1;
+    }
+    
+    return valor;
+}
+
+const valor = valorCarta(pedirCarta());
+console.log(valor);
